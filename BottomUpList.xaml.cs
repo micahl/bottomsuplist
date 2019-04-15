3b4bc54bbd28a89495c0a913629e0380d0646f91
Micah Lewis <micahl@microsoft.com>
2019-04-15 12:02:07 -0700

    Init

---
+++ BottomUpList.xaml.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BottomsUpList
+{
+    public sealed partial class BottomUpList : UserControl
+    {
+        private bool _loadingMoreItems;
+
+        public BottomUpList()
+        {
+            this.InitializeComponent();
+
+            //this.Loaded += BottomUpList_Loaded;
+        }
+
+        private void BottomUpList_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Scroller != null)
+            {
+                // Scroll to the end.
+                Scroller.ScrollTo(
+                    Scroller.HorizontalOffset, 
+                    Scroller.ExtentHeight - Scroller.ViewportHeight,
+                    new ScrollOptions(AnimationMode.Disabled));
+            }
+        }
+
+        private bool LoadingMoreItems
+        {
+            get { return (bool)GetValue(LoadingMoreItemsProperty); }
+            set { SetValue(LoadingMoreItemsProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        public object ItemsSource
+        {
+            get { return (object)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        private static readonly DependencyProperty LoadingMoreItemsProperty =
+            DependencyProperty.Register(
+                nameof(LoadingMoreItems),
+                typeof(bool),
+                typeof(BottomUpList),
+                new PropertyMetadata(false));
+
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register(
+                nameof(ItemTemplate),
+                typeof(DataTemplate),
+                typeof(BottomUpList),
+                new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(
+                nameof(ItemsSource),
+                typeof(object),
+                typeof(BottomUpList),
+                new PropertyMetadata(null));
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            // Allow the panel to arrange first
+            var result = base.ArrangeOverride(finalSize);
+
+            //LoadMoreItems(20);
+
+            return result;
+        }
+
+        private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            if (!e.IsIntermediate)
+            {
+                var itemsSource = ItemsSource as ISupportIncrementalLoading;
+                var scroller = (Windows.UI.Xaml.Controls.ScrollViewer)sender;
+                //var distanceToTop = scroller.ExtentHeight - (scroller.VerticalOffset + scroller.ViewportHeight);
+
+                // trigger if within 2 viewports of the top
+                if (scroller.VerticalOffset <= 2.0 * scroller.ViewportHeight
+                        && itemsSource.HasMoreItems && !LoadingMoreItems)
+                {
+                    LoadMoreItems(15);
+                }
+            }
+        }
+
+        private void ScrollViewer_ViewChanged(Microsoft.UI.Xaml.Controls.Primitives.Scroller scroller, object args)
+        {
+            var itemsSource = ItemsSource as ISupportIncrementalLoading;
+
+            if (scroller.State == Microsoft.UI.Xaml.Controls.InteractionState.Idle)
+            {
+                // trigger if within 2 viewports of the top
+                if (scroller.VerticalOffset <= 2.0 * scroller.ViewportHeight
+                        && itemsSource.HasMoreItems && !LoadingMoreItems)
+                {
+                    LoadMoreItems(15);
+                }
+            }
+        }
+
+        private async void LoadMoreItems(int dataFetchSize)
+        {
+            var itemsSource = ItemsSource as ISupportIncrementalLoading;
+            if (itemsSource == null) return;
+
+            // show an indeterminate progress UI
+            LoadingMoreItems = true;
+
+            await itemsSource.LoadMoreItemsAsync(/*DataFetchSize*/ (uint)dataFetchSize);
+
+            LoadingMoreItems = false;
+        }
+
+        //protected override void OnKeyDown(KeyRoutedEventArgs e)
+        //{
+        //    double verticalOffset = 0;
+        //    switch (e.Key)
+        //    {
+        //        case Windows.System.VirtualKey.Up:
+        //            verticalOffset = Scroller.VerticalOffset - Scroller.ViewportHeight / 8;
+        //            break;
+        //        case Windows.System.VirtualKey.Down:
+        //            verticalOffset = Scroller.VerticalOffset + Scroller.ViewportHeight / 8;
+        //            break;
+        //        case Windows.System.VirtualKey.End:
+        //            verticalOffset = Scroller.ExtentHeight - Scroller.ViewportHeight;
+        //            break;
+        //        case Windows.System.VirtualKey.Home:
+        //            verticalOffset = 0;
+        //            break;
+        //        case Windows.System.VirtualKey.PageUp:
+        //            verticalOffset = Scroller.VerticalOffset - Scroller.ViewportHeight;
+        //            break;
+        //        case Windows.System.VirtualKey.PageDown:
+        //            verticalOffset = Scroller.VerticalOffset + Scroller.ViewportHeight;
+        //            break;
+        //        default:
+        //            base.OnKeyDown(e);
+        //            break;
+        //    }
+
+        //    if (verticalOffset >= 0)
+        //    {
+        //        //Scroller.ChangeView(null, verticalOffset, null);
+        //        //Scroller.ScrollTo(Scroller.HorizontalOffset, verticalOffset);
+        //    }
+        //}
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BottomsUpList
+{
+    // Bindable class representing a single text message.
+    public class TextMessage
+    {
+        public string Body { get; set; }
+        public string DisplayTime { get; set; }
+        public bool IsSent { get; set; }
+        public bool IsReceived { get { return !IsSent; } }
+        public IList<object> Placeholders { get; set; }
+    }
+
+    // Observable collection representing a text message conversation
+    // that can load more items incrementally.
+    public class Conversation : ObservableCollection<TextMessage>, ISupportIncrementalLoading
+    {
+        private uint messageCount = 0;
+
+        public Conversation()
+        {
+        }
+
+        public bool HasMoreItems { get; } = true;
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            this.CreateMessages(count);
+
+            return Task.FromResult<LoadMoreItemsResult>(
+                new LoadMoreItemsResult()
+                {
+                    Count = count
+                }).AsAsyncOperation();
+        }
+
+        private void CreateMessages(uint count)
+        {
+            for (uint i = 0; i < count; i++)
+            {
+                this.Insert(0, new TextMessage()
+                {
+                    Body = $"{messageCount}: {CreateRandomMessage()}",
+                    IsSent = 0 == messageCount++ % 2,
+                    DisplayTime = DateTime.Now.ToString(),
+                    Placeholders = CreateRandomPlaceholders()
+                });
+            }
+        }
+
+        private static Random rand = new Random();
+        private static string fillerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+        public static string CreateRandomMessage()
+        {
+            return fillerText.Substring(0, rand.Next(5, fillerText.Length));
+        }
+
+        public static IList<object> CreateRandomPlaceholders()
+        {
+            int count = rand.Next(0, 5);
+            var list = new List<object>();
+            for (int i = 0; i < count; i++)
+                list.Add(null);
+
+            return list;
+        }
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        private readonly Conversation conversation = new Conversation();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //this.conversation.LoadMoreItemsAsync((int)10).GetResults();
+            chatView.ItemsSource = this.conversation;
+        }
+
+        public static Windows.UI.Xaml.HorizontalAlignment GetItemAlignment(bool isSent)
+        {
+            return isSent ? Windows.UI.Xaml.HorizontalAlignment.Right : Windows.UI.Xaml.HorizontalAlignment.Left;
+        }
+
+        async void SendTextMessage()
+        {
+            if (MessageTextBox.Text.Length > 0)
+            {
+                this.conversation.Add(new TextMessage
+                {
+                    Placeholders = Conversation.CreateRandomPlaceholders(),
+                    Body = MessageTextBox.Text,
+                    DisplayTime = DateTime.Now.ToString(),
+                    IsSent = true
+                });
+                MessageTextBox.Text = string.Empty;
+
+                // Send a simulated reply after a brief delay.
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+                this.conversation.Add(new TextMessage
+                {
+                    Placeholders = Conversation.CreateRandomPlaceholders(),
+                    Body = Conversation.CreateRandomMessage(),
+                    DisplayTime = DateTime.Now.ToString(),
+                    IsSent = false
+                });
+            }
+        }
+
+        private void MessageTextBox_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter)
+            {
+                this.SendTextMessage();
+            }
+        }
+    }
+}
